fc29c519b026cda972d9de135f9918f8343f563e
aleksandar83p <alex5ko83@gmail.com>
2021-01-21 16:06:55 +0100

    Add state to the main component

--- src/components/MainMenu/MainMenu.tsx
+++ src/components/MainMenu/MainMenu.tsx
@@ -15,12 +15,39 @@ interface MainMenuProperties{
     items: MainMenuItem[];
 }
 
+interface MainMenuState{
+    items: MainMenuItem[];
+}
+
 export class MainMenu extends React.Component<MainMenuProperties>{
+    state: MainMenuState;
+
+    constructor(props: Readonly<MainMenuProperties>){
+        super(props);
+
+        this.state = {
+            items: props.items,
+        };
+
+        setInterval( () => {
+            const novaLista = [...this.state.items];
+            novaLista.push(new MainMenuItem("Naslov", "/link"));
+            this.setItems(novaLista);             
+        }, 2000);
+    }
+
+    setItems( itemsArg: MainMenuItem[]){
+        this.setState({
+            items: itemsArg
+        });
+
+    }
+
     render(){
         return (
             <Container>
                 <Nav variant="tabs">
-                    { this.props.items.map(this.makeNavLink) }              
+                    { this.state.items.map(this.makeNavLink) }              
                 </Nav>
             </Container>            
         );        
